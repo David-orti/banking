f5b37989ed12534951c14d1cf12250765f8c5f9f
jorge ortiz <jorgedavidortiz20@gmail.com>
2025-09-08 12:19:45 -0500

    models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class   User(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20, blank=True)
+    email = models.CharField(max_length=150)
+     
+     
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    abrev = models.CharField(max_length=10)
+
+
+
+
+
+
+
+
+
+
--- banking/settings.py
+++ banking/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authentication',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,16 +75,16 @@ WSGI_APPLICATION = 'banking.wsgi.application'
 
 DATABASES = {
  
-      'default': {
+      'local': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': 'localhost',
         'NAME': 'banking',
         'USER': 'postgres',
         'PASSWORD': 'unicesmag',
-        'PORT': '5432',
+        'PORT': '5433',
       },
 
-    'local': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'banking.sqlite3',
     }
